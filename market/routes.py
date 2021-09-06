155441ca9f5998aeae6e143e6f3151b0f0b52ba0
philwoz <61164019+philwoz@users.noreply.github.com>
2021-09-06 21:20:25 +0100

    added validataions on  registration form

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for
+from market.models import Item , User
 from market.forms import RegisterForm
+from market import db
 
 
 @app.route('/')
@@ -15,7 +16,18 @@ def market_page():
     item = Item.query.all()
     return render_template('market.html', items=item)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'error creating user: {err_msg}')
+
     return render_template('register.html', form=form)
