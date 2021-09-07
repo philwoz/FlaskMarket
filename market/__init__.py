286868e0b44c8f4ed16ba282b158a9593eb4f346
philwoz <61164019+philwoz@users.noreply.github.com>
2021-09-07 19:42:11 +0100

    hash passwords with brypt

--- market/__init__.py
+++ market/__init__.py
@@ -1,13 +1,15 @@
 from enum import unique
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from market.hidden import secret_key
+from flask_bcrypt import Bcrypt
 s_key = secret_key()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = s_key
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 from market import routes
 
 
